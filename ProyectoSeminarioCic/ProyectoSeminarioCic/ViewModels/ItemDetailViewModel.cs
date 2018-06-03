209500e5789f89fc6d2d81ca3f1dc4881afc3b7d
dhamarmj <39349434+dhamarmj@users.noreply.github.com>
2018-06-03 08:51:54 -0400

    Agregar archivos de proyecto.

---
+++ ProyectoSeminarioCic/ProyectoSeminarioCic/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ProyectoSeminarioCic.Models;
+
+namespace ProyectoSeminarioCic.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
