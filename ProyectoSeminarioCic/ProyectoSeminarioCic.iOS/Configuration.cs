d8f9ea28dad021d4ceae9b648c39cf749ddfffd6
dhamarmj <39349434+dhamarmj@users.noreply.github.com>
2018-06-04 17:21:55 -0400

    Progreso
    Vista Login, Modelo Usuario base, implementacion de Controladores para DBinterna, Nugets SQLite y SQLite.Net

---
+++ ProyectoSeminarioCic/ProyectoSeminarioCic.iOS/Configuration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using SQLite.Net.Interop;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ProyectoSeminarioCic.iOS.Configuration))]
+
+
+namespace ProyectoSeminarioCic.iOS
+{
+    public class Configuration : ProyectoSeminarioCic.Services.IConfiguration
+    {
+        private string Directorio;
+        private ISQLitePlatform Plataforma;
+
+        public string directorio
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Directorio))
+                {
+                    var dir = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                    Directorio = Path.Combine(dir,"..","Library");
+                }
+                return Directorio;
+            }
+        }
+
+        public ISQLitePlatform plataforma
+        {
+            get
+            {
+                if (Plataforma == null)
+                {
+                    Plataforma = new SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS();
+                }
+                return Plataforma;
+            }
+        }
+    }
+}
---
+++ ProyectoSeminarioCic/ProyectoSeminarioCic/Views/Login.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoSeminarioCic.Views
+{
+	
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+		}
+
+        private void BtnIniciar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
