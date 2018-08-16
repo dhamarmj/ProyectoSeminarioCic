84308dd110892904ac29ae22593bc4cbf57d5874
JOSHUA DÍAZ CEPEDA <20151712@ce.pucmm.edu.do>
2018-08-15 20:52:23 -0400

    Probando

---
+++ ProyectoSeminarioCic/ProyectoSeminarioCic/ViewModels/ClasePrueba.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoSeminarioCic.ViewModels
+{
+    class ClasePrueba
+    {
+        // prueba pal github
+    }
+}
