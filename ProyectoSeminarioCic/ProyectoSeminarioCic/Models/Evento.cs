0e16f9abc1dd6ad24ae1dd85b191c622708a6831
EDGAR JARET NÚÑEZ PERDOMO <20150166@ce.pucmm.edu.do>
2018-08-26 07:39:41 -0400

    Update a View HorarioActividades

--- ProyectoSeminarioCic/ProyectoSeminarioCic/Models/Evento.cs
+++ ProyectoSeminarioCic/ProyectoSeminarioCic/Models/Evento.cs
@@ -7,42 +7,23 @@ namespace ProyectoSeminarioCic.Models
     // [Table("Evento")]
     public class Evento
     {
-
-        //  [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Descripcion { get; set; }
         public DateTime Fecha { get; set; }
-        public TimeSpan TSHora { get; set; }
         public string Ubicacion { get; set; }
-        public string Duracion { get; set; }
-        public string Comentario { get; set; }
+        public double Duracion { get; set; }
+        public DateTime FechaFin { get; set; }
         public string Titulo { get; set; }
-        public string RangoHora
-        {
-            get
-            {
-                if (TSHora != null && Duracion != string.Empty)
-                {
-                    var time = TimeSpan.FromMinutes(Convert.ToInt32(Duracion));
-                    if (time.Equals(0))
-                        return string.Format("Desde: {0}", TSHora.ToString(@"hh\:mm"));
-                    else
-                        return string.Format("Desde: {0} Hasta: {1}", TSHora.ToString(@"hh\:mm"), TSHora.Add(time).ToString(@"hh\:mm"));
-                }
-                else
-                   return string.Format("Desde: {0}", TSHora);
-            }
-        }
-
-        public Evento(string desc)
-        {
-            Descripcion = desc;
-
-
-        }
-
+        public int Id_seminario { get; set; }
+        
         public Evento()
         {
+            // Fecha.ToString("hh");
+            Fecha = new DateTime();
+            Duracion = 0;
+            FechaFin = Fecha.AddMinutes(Duracion);
         }
+
+        public string NewProperty => string.Format("{0} - {1} ", Fecha, FechaFin);
     }
 }
